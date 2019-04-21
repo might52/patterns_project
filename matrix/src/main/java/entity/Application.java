b9ea6588ad905d05e76eab9256543485db6bfe93
Andrei F <fas@live.ru>
2019-04-22 00:48:46 +0300

    Task 4 - added HorizontalGroupMatrix. Added groups, realized set and get values, cols and row amount, printer, addMatrix

--- matrix/src/main/java/entity/Application.java
+++ matrix/src/main/java/entity/Application.java
@@ -1,16 +1,13 @@
 package entity;
 
-import entity.Impl.Matrix.ComposedMatrix;
+import entity.Impl.Matrix.HorizontalGroupMatrix;
 import entity.Impl.Matrix.ReorderDecorator;
 import entity.Impl.Matrix.ThinMatrix;
 import entity.Impl.Matrix.UsualMatrix;
 import entity.Impl.Printers.ConsolePrinter;
 import entity.Interfaces.IMatrix;
 
-import javax.swing.*;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Application {
 
@@ -57,13 +54,46 @@ public class Application {
         thinMatrix.print(new ConsolePrinter(true));
     }
 
+    private static void setValue(IMatrix matrix, double value){
+        for (int i = 0; i < matrix.getRowsAmount(); i++) {
+            for (int j = 0; j < matrix.getColumnsAmount(); j++) {
+                matrix.setValue(i, j, value);
+            }
+        }
+    }
+
     private static void fourthTask() {
-        IMatrix thinMatrix = new ThinMatrix(2,3);
-        InitMatrix.fillMatrix(thinMatrix, 3,6);
-        IMatrix usualMatrix = new UsualMatrix(2,3);
-        InitMatrix.fillMatrix(usualMatrix, 3,6);
-        ComposedMatrix composedMatrix = new ComposedMatrix(Arrays.asList(usualMatrix, thinMatrix));
-        composedMatrix.print(new ConsolePrinter(true));
+        IMatrix thinMatrix = new ThinMatrix(2,2);
+        InitMatrix.fillMatrix(thinMatrix, 4,1);
+        IMatrix usualMatrix1 = new UsualMatrix(2,2);
+        InitMatrix.fillMatrix(usualMatrix1, 4,2);
+//        IMatrix usualMatrix2 = new UsualMatrix(3,1);
+//        InitMatrix.fillMatrix(usualMatrix2, 3,3);
+//        IMatrix usualMatrix3 = new UsualMatrix(3,1);
+//        InitMatrix.fillMatrix(usualMatrix3, 3,4);
+
+        setValue(thinMatrix, 1);
+        setValue(usualMatrix1, 2);
+//        setValue(usualMatrix2, 3);
+//        setValue(usualMatrix3, 1.0);
+
+        HorizontalGroupMatrix horizontalGroupMatrix = new HorizontalGroupMatrix(
+                Arrays.asList(
+                        thinMatrix,
+                        usualMatrix1
+//                        usualMatrix2
+                )
+        );
+
+//        MatrixStats statHorizontalGroupMatrix = new MatrixStats(horizontalGroupMatrix);
+//        statHorizontalGroupMatrix.printStats();
+
+        horizontalGroupMatrix.print(new ConsolePrinter(true));
+        horizontalGroupMatrix.setValue(1,0, 4);
+        horizontalGroupMatrix.setValue(1,1, 4);
+        horizontalGroupMatrix.setValue(1,2, 4);
+        horizontalGroupMatrix.setValue(1,3, 4);
+        horizontalGroupMatrix.print(new ConsolePrinter(true));
     }
 }
 
